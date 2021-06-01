44a55eb3838b1831179380a971ea1fe8a652852d
indragunawanzai <nawanzai@gmail.com>
2021-06-01 16:14:21 +0700

    menambahkan file koneksi

---
+++ server.js
@@ -0,0 +1,17 @@
+/* 
+    variabel express untuk memanggil library express
+    variabel bodyParser memanggil library body-parser
+    app => fungsi secara global untuk memanggil express js
+*/
+
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
+const app = express();  
+
+//parse application/json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log(`Server started on port`);
+});
